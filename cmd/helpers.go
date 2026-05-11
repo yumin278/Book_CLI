3b08f0d8b2df2fc3761f787be3ed96dc48c8d6fc
Lobstery_v2 <lobsteryv2@gmail.com>
2026-05-11 21:51:13 +0800

    fix(helpers): use os.Stdout and fmt.Fprintln for CLI output and handle errors

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -37,13 +37,17 @@ func printResponse(raw []byte) error {
 				sanitized = append(sanitized, ' ')
 			}
 		}
-		fmt.Println(string(sanitized))
+		if _, err := fmt.Fprintf(os.Stdout, "%s\n", sanitized); err != nil {
+			return err
+		}
 		return nil
 	}
 
 	var parsed any
 	if err := json.Unmarshal(raw, &parsed); err != nil {
-		fmt.Println(string(raw))
+		if _, err := fmt.Fprintf(os.Stdout, "%s\n", raw); err != nil {
+			return err
+		}
 		return nil
 	}
 
@@ -64,8 +68,10 @@ func printValue(v any) error {
 	if err != nil {
 		return err
 	}
-
-	fmt.Println(string(out))
+ 
+	if _, err := fmt.Fprintf(os.Stdout, "%s\n", out); err != nil {
+		return err
+	}
 	return nil
 }
 
